ae30435b562251706c73c07e2e54325d27a23c19
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-19 15:01:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+var_dump('prova');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+  <header></header>
+
+  <body>
+
+  </body>
+</body>
+
+</html>
