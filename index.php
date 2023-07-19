b5f0ce04ae8460bd3c670096e3487e579e340ab6
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-19 15:20:46 +0200

    struttura del form

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-var_dump('prova');
+
 
 ?>
 
@@ -12,17 +12,24 @@ var_dump('prova');
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Random Password Generator</title>
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
-<body>
-  <header></header>
-
-  <body>
-
-  </body>
+<body class="bg-dark text-light">
+  <header class="text-center mt-5">
+    <h1>Random Password Generator</h1>
+  </header>
+  <main class="container">
+    <form class="mx-5">
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Numero Caratteri</label>
+        <input type="number" class="form-control" id="exampleInputPassword1">
+      </div>
+      <button type="submit" class="btn btn-light">Conferma</button>
+    </form>
+  </main>
 </body>
 
 </html>
